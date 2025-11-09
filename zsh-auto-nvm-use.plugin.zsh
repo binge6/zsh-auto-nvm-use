cf77952fbb5525dab6454f27641cb9e0fe684a53
Braden <1830020547@qq.com>
2025-11-09 23:20:51 +0800

    feat: Check for .node-version file

--- zsh-auto-nvm-use.plugin.zsh
+++ zsh-auto-nvm-use.plugin.zsh
@@ -1,5 +1,20 @@
 # place this after nvm initialization!
 autoload -U add-zsh-hook
+
+nvm_find_nvmrc() {
+  local dir
+  dir="$(nvm_find_up '.nvmrc')"
+  if [ -e "${dir}/.nvmrc" ]; then
+    nvm_echo "${dir}/.nvmrc"
+    return
+  fi
+  # Check for .node-version file
+  dir="$(nvm_find_up '.node-version')"
+  if [ -e "${dir}/.node-version" ]; then
+    nvm_echo "${dir}/.node-version"
+  fi
+}
+
 load-nvmrc() {
   local node_version="$(nvm version)"
   local nvmrc_path="$(nvm_find_nvmrc)"
